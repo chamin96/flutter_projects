3705b399e8c5a83b84f511719b3a36e5808dbf2d
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-11-19 13:04:04 +0530

    time app init

---
+++ world_time/android/app/src/main/java/com/knztnt/world_time/MainActivity.java
@@ -0,0 +1,6 @@
+package com.knztnt.world_time;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ world_time/lib/pages/choose_location.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0 ,
+      ),
+      body: Text('choose location'),
+    );
+  }
+}
