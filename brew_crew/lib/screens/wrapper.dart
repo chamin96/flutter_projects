5bb95914ce1c80baca8e5b91a53bc4cbaf9ef664
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-11-21 11:09:24 +0530

    basic screen setup

---
+++ brew_crew/lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:brew_crew/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    // return either Home or Authentication screen
+    return Home();
+  }
+}
