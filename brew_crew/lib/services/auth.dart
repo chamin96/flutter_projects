38a071f41bf4be5c0fa6fa1df749330a1f5701b1
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-11-22 13:25:16 +0530

    user model creation

--- brew_crew/lib/services/auth.dart
+++ brew_crew/lib/services/auth.dart
@@ -1,8 +1,14 @@
+import 'package:brew_crew/models/user_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
 
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create a user object
+  UserModel _userFirebase (User user) {
+    return user != null ? UserModel(uid: user.uid) : null;
+  }
 
   // sign in anonymously
   Future signInAnon() async {
@@ -10,7 +16,7 @@ class AuthService {
     try {
       UserCredential userCredential = await _auth.signInAnonymously();
       User user = userCredential.user;
-      return user;
+      return _userFirebase(user);
     } catch(e) {
       print(e.toString());
       return null;
