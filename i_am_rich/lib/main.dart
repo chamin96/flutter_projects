758bf3ea8c45a9558e8e6bdaefb89aaf49d3a88d
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-12-30 15:40:21 +0530

    i_am_rich mini project

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text('I am Rich'),
+          ),
+          backgroundColor: Colors.blueGrey[800],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
