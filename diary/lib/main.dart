89b89f58a8dbf966d82f63cdef2f844568052012
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2021-01-11 23:57:11 +0530

    initial commit

---
+++ diary/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'contact_page.dart';
+
+void main() async {
+  final appDocumentDirectory =
+      await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Diary',
+      home: FutureBuilder(
+        future: Hive.openBox('contacts'),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Text(snapshot.error.toString());
+            } else {
+              return ContactPage();
+            }
+          }
+          // Although opening a Box takes a very short time,
+          // we still need to return something before the Future completes.
+          else
+            return Scaffold();
+        },
+      ),
+    );
+  }
+}
