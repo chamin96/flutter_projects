6b5dc5017181258811783a1e3f14fe403c88e678
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2021-01-15 23:59:39 +0530

    dispose method call

--- diary/lib/main.dart
+++ diary/lib/main.dart
@@ -11,7 +11,12 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -33,5 +38,10 @@ class MyApp extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void dispose() {
+    Hive.close();
+    super.dispose();
   }
 }
