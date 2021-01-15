8ffbdd5895d6b061fdaac8513c0a991aff09f361
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2021-01-15 23:49:12 +0530

    Unhandled Exception: ServicesBinding.defaultBinaryMessenger was accessed before the binding was initialized. [bug fixed]

--- diary/lib/main.dart
+++ diary/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:path_provider/path_provider.dart' as path_provider;
 import 'contact_page.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   final appDocumentDirectory =
       await path_provider.getApplicationDocumentsDirectory();
   Hive.init(appDocumentDirectory.path);
