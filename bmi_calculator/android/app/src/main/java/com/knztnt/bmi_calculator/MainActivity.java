4b3177204ed604d9e1c0ca9ba3d8ed486965e2da
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2021-01-26 15:17:14 +0530

    initial setup

---
+++ bmi_calculator/android/app/src/main/java/com/knztnt/bmi_calculator/MainActivity.java
@@ -0,0 +1,6 @@
+package com.knztnt.bmi_calculator;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ bmi_calculator/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'input_page.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: Color(0xFF0A0E21), // navigation bar color
+    systemNavigationBarIconBrightness: Brightness.dark, //navigation bar icon
+  ));
+  // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+  // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+  runApp(BMICalculator());
+}
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      darkTheme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        accentColor: Colors.amber,
+        scaffoldBackgroundColor: Color(0xFF1D2136),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.amber,
+        ),
+      ),
+      theme: ThemeData.light().copyWith(
+        primaryColor: Colors.amber,
+        accentColor: Color(0xFF0A0E21),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Color(0xFF0A0E21),
+        ),
+      ),
+      home: InputPage(),
+    );
+  }
+}
