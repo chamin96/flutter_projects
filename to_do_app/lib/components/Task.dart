3fa4714eb82f0f4832c5ddc39f8bbe5d3ede4316
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2021-02-15 19:54:20 +0530

    Update Task.dart

--- to_do_app/lib/components/Task.dart
+++ to_do_app/lib/components/Task.dart
@@ -6,22 +6,32 @@ class Task extends StatefulWidget {
 }
 
 class _TaskState extends State<Task> {
+  bool isDone = false;
+  String taskTitle = 'This is todo title';
+  String taskDesc = 'This is todo description(optional)';
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
-        'This is todo title',
+        taskTitle,
         style: TextStyle(
           fontSize: 22.0,
         ),
       ),
-      subtitle: Text('This is todo description(optional)'),
+      subtitle: Text(taskDesc),
       trailing: IconButton(
-          icon: Icon(
-            Icons.done_sharp,
-            size: 32.0,
-          ),
-          onPressed: () => {print('done')}),
+        icon: Icon(
+          Icons.done_sharp,
+          color: isDone ? Colors.indigoAccent : Colors.blueGrey,
+          size: 32.0,
+        ),
+        onPressed: () {
+          setState(() {
+            isDone = !isDone;
+          });
+        },
+      ),
     );
   }
 }
