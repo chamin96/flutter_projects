2bd20a119c1bfb0dc1ef590fa561873f5d69e13e
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2021-02-15 19:54:30 +0530

    Update main.dart

--- to_do_app/lib/main.dart
+++ to_do_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './components/Task.dart';
+import './pages/HomePage.dart';
 
 void main() {
   runApp(ToDoApp());
@@ -22,117 +22,7 @@ class ToDoApp extends StatelessWidget {
         splashColor: Color(0xFFA7D0B8),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(title: ''),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        title: Text(
-          widget.title,
-          style: TextStyle(
-            color: Color(0xFF5D61A2),
-            fontSize: 32.0,
-          ),
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.assignment_turned_in,
-              color: Color(0xFF5D61A2),
-              size: 30.0,
-            ),
-            onPressed: () => {},
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.more_vert_sharp,
-              color: Color(0xFF5D61A2),
-              size: 30.0,
-            ),
-            onPressed: () => {},
-          ),
-        ],
-        elevation: 0.0,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListView(
-          children: <Widget>[
-            Text(
-              'Today',
-              style: TextStyle(
-                color: Theme.of(context).primaryColor,
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Column(
-              children: <Widget>[
-                Task(),
-                Task(),
-                Task(),
-                Task(),
-                Task(),
-                Task(),
-              ],
-            ),
-            Text(
-              'Tomorrow',
-              style: TextStyle(
-                color: Theme.of(context).primaryColor,
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Column(
-              children: <Widget>[
-                Task(),
-                Task(),
-                Task(),
-                Task(),
-                Task(),
-                Task(),
-              ],
-            ),
-            Text(
-              'Upcoming',
-              style: TextStyle(
-                color: Theme.of(context).primaryColor,
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      home: HomePage(),
     );
   }
 }
